9b595f345e892cdee9569c270e223e8540e18776
diogoku <diogomnribeiro@gmail.com>
2020-10-15 21:20:22 +0100

    Deploy project on Github pages and remove counterSlice template

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import countriesReducer from "../features/countries/countriesSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     countries: countriesReducer,
   },
 });
