df80b0981f5018344ac7a1d9399c47c2668e9432
diogoku <diogomnribeiro@gmail.com>
2020-10-15 21:18:46 +0100

    Updates

---
+++ precache-manifest.704d8143a166b8c952cb89749267f893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb28e3742c6005e359456173f0964a5",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "27a08af3dca6d77c797b",
+    "url": "/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "18d3070e7f56c1c22dc2",
+    "url": "/covid-19-tracker/static/css/main.2311e5b5.chunk.css"
+  },
+  {
+    "revision": "27a08af3dca6d77c797b",
+    "url": "/covid-19-tracker/static/js/2.d5953433.chunk.js"
+  },
+  {
+    "revision": "a187be6633db8f1dea047f069727b2da",
+    "url": "/covid-19-tracker/static/js/2.d5953433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d3070e7f56c1c22dc2",
+    "url": "/covid-19-tracker/static/js/main.ef6abdcd.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
